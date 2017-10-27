1c69cdd579f9bdd44c677ec1e200c28ecbf71a48
fawazahmed86 <honorfas@gmail.com>
2017-10-27 10:03:48 -0700

    changes update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,20 @@
 
 var friendsList = [
   {
-    name: "Spongebob Squarepants",
-    scores: [ 4, 2, 5, 3, 4, 2, 2, 2, 5, 4]
-  }, {
-    name: "Tom from Tom & Jerry",
-    scores: [ 5, 3, 5, 3, 3, 1, 5, 5, 5, 2]
-  }, {
     name: "Courage the Cowardly Dog",
     scores: [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  }, {
+    name: "Spongebob Squarepants",
+    scores: [ 4, 3, 5, 3, 4, 2, 2, 2, 5, 4]
+  }, {
+    name: "Tom from Tom & Jerry",
+    scores: [ 5, 3, 2, 1, 2, 3, 1, 2, 3, 3]
   }, {
     name: "Dennis the menace",
     scores: [ 5, 1, 5, 1, 5, 2, 1, 1, 5, 5]
   }, {
     name: "Jonh Cena",
-    scores: [ 5, 3, 2, 1, 2, 3, 1, 2, 3, 3]
+    scores: [ 5, 3, 5, 3, 3, 1, 5, 5, 5, 2]
   }
 ];
 
